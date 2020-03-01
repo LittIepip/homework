de8428192a2e509ea133c50c9c063941c065b06c
LittIepip <misteriumss@gmail.com>
2020-03-01 20:16:38 +0300

    HomeWork3.by ulitnikov

---
+++ HomeWork3/src/by/ulitnikov/Main.java
@@ -0,0 +1,13 @@
+package by.ulitnikov;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int i = 65161847;
+	if(i % 10 == 7){
+        System.out.println("Семерка на конце числа : " + i + " присутствует ");
+    } else {
+        System.out.println("Семерка на конце числа : " + i + " отсутствует ");
+        }
+    }
+}
